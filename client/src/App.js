47d32434adbcec4eb86ed068f3f1ad393a3dc827
Zijie Li <michael20001122@gmail.com>
2023-12-23 22:35:40 -0500

    2

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
+
+  const [data, setMessage] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+        data => 
+        setMessage(data))
+        console.log(data)
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        (typeof data.members === 'undefined') ? (
+          <p>Loading...</p>
+        )   : (
+          data.members.map((member, index) => (
+            <p key={index}>{member}</p>
+          ))
+        )     
+      }
     </div>
   );
 }
