c4d403bbe6a4037326512f20dcd63fb21d092b1e
Zijie Li <michael20001122@gmail.com>
2023-12-27 21:38:38 +0800

    comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './App.css'; // 假设你的CSS文件名是ChatInterface.css
+import './App.css'; 
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
-  const [reportUrl, setReportUrl] = useState(''); // 新状态来保存报告的URL
+  const [reportUrl, setReportUrl] = useState(''); 
   const [jobSearchResults, setJobSearchResults] = useState([]);
   const [emailTemplate, setEmailTemplate] = useState('');
 
@@ -16,7 +16,7 @@ const App = () => {
       sender: 'user',
     };
   
-    // 解析用户消息中的角色、行业和地点
+    // extract the role, industry, and location from the message
     const roleMatch = inputMessage.match(/\[ROLE: (.*?)\]/);
     const industryMatch = inputMessage.match(/\[Industry: (.*?)\]/);
     const locationMatch = inputMessage.match(/\[LOCATION: (.*?)\]/);
@@ -25,19 +25,18 @@ const App = () => {
     const industry = industryMatch ? industryMatch[1] : null;
     const location = locationMatch ? locationMatch[1] : null;
   
-    // 定义AI的回复消息
     let aiMessage;
   
-    // 检查是否所有所需信息都已提取
+    // check the message
     if (role && industry && location) {
       try {
-        // 这里发送请求到您的后端
+        // send the response to backend via axios
         const response = await axios.post('/search_jobs', { role, industry, location });
         aiMessage = {
           text: response.data.ai_message,
           sender: 'ai',
         };
-        setJobSearchResults(response.data.jobs_data);  // 存储工作搜索结果
+        setJobSearchResults(response.data.jobs_data);  // store the jobs data in state
       } catch (error) {
         console.error('Error sending message:', error);
         aiMessage = {
@@ -52,18 +51,19 @@ const App = () => {
       };
     }
   
-    // 更新消息列表以包括用户的消息和AI的响应
+    
     setMessages(prevMessages => [...prevMessages, userMessage, aiMessage]);
   
-    // 清空输入区域
+    
     setInputMessage('');
   };
   
   const handleCreateEmails = async () => {
+    // hard code for 'designers'
     const designers = ['John Smith', 'Jane Doe', 'Bob Johnson', 'Mary Jane', 'James Bond']
     try {
       const response = await axios.post('/create_emails', { email_template: emailTemplate, designers: designers });
-      setReportUrl(response.data.report_url);  // 假设后端返回的是PDF下载链接
+      setReportUrl(response.data.report_url);  
     } catch (error) {
       console.error('Error creating emails:', error);
     }
--- flask-server/server.py
+++ flask-server/server.py
@@ -5,15 +5,13 @@ from reportlab.lib.pagesizes import letter
 from io import BytesIO
 import os
 
-# Initialize Flask-Migrate
 
 app = Flask(__name__)
 
-# 确保reports文件夹存在，并且是在Flask应用的根目录下
 reports_dir = os.path.join(app.root_path, 'reports')
 os.makedirs(reports_dir, exist_ok=True)
 
-#初始化Database
+# initialize database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobseeker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -23,21 +21,19 @@ class Company(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     industry = db.Column(db.String(120), nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
-    roles = db.relationship('Role', backref='company', lazy='dynamic')  # 假设一家公司有多个角色
+    roles = db.relationship('Role', backref='company', lazy='dynamic')  
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)  # 关联到公司
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)  
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(80), nullable=False)
-    companies = db.relationship('Company', backref='location', lazy='dynamic')  # 假设一个地点有多家公司
+    companies = db.relationship('Company', backref='location', lazy='dynamic')  
 
 def get_company_details(company_name):
-    # 这里应该是获取公司详细信息的逻辑，例如网站URL和设计师列表
-    # 以下是示例数据，您需要替换成实际的数据获取逻辑
     website = 'https://www.{}.com'.format(company_name)
     designers = ['John Smith', 'Jane Doe', 'Bob Johnson', 'Mary Jane', 'James Bond']
     return website, designers
@@ -52,7 +48,7 @@ def search_jobs():
     industry_type = data.get('industry')
     location_city = data.get('location')
     
-    # 根据角色、行业和城市进行搜索
+    # search for jobs from the database
     jobs = Role.query \
         .join(Company) \
         .join(Location) \
@@ -62,13 +58,12 @@ def search_jobs():
             Location.city.ilike(f'%{location_city}%')
         ).all()
     
-    # 将查询结果格式化为JSON
     companies_details = [get_company_details(job.company.name) for job in jobs]
 
-    # 构建AI的回复消息
+    # initialize the ai message
     ai_message = "Okay, you should apply to {}. Here are links to their websites, with the product design roles. ".format(", ".join([job.company.name for job in jobs]))
     joblistmsg = ''
-    # 添加公司网站URL和设计师列表到AI的回复
+    
     for company, details in zip(jobs, companies_details):
         website_url, designers_list = details
         # ai_message += "For {}, here is the website: {}. ".format(company.company.name, website_url)
@@ -76,7 +71,7 @@ def search_jobs():
 
     ai_message += "Here are 5 designers at each of these companies, which I have added these to a list called Design Job Search. Would you like me to send emails to these people, mentioning your interest in the position?"
 
-    # 将AI的回复和查询结果格式化为JSON
+    
     response_data = {
         'ai_message': ai_message,
         'jobs_data': [{
@@ -98,30 +93,29 @@ def create_emails():
     email_template = data.get('email_template')
     designers = data.get('designers')
     
-    # 文件路径
+    # file path
     filename = "personalized_emails.pdf"
     file_path = os.path.join(reports_dir, filename)
 
-    # 创建一个PDF文件并保存到文件系统
+    # create the pdf file
     pdf = canvas.Canvas(file_path, pagesize=letter)
     width, height = letter
 
-    # 为每个设计师生成邮件并写入PDF
-    y_position = height - 40  # 开始位置
+    # overwrite for every designer
+    y_position = height - 40  # begin position
     for designer in designers:
         personalized_email = email_template.replace('Ben', designer.split(' ')[0])
         pdf.drawString(40, y_position, personalized_email)
-        y_position -= 40  # 更新y坐标以获取下一个位置
+        y_position -= 40  # next position
     pdf.save()
     
     report_url = f"http://{request.host}/reports/{os.path.basename(file_path)}"
 
-    # 提供一个URL以供下载
+    # store the download url
     return jsonify({
         'report_url': report_url
     })
 
-# 确保有一个路由来提供reports目录中的文件
 @app.route('/reports/<filename>')
 def report(filename):
     return send_from_directory(reports_dir, filename)
