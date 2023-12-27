ea8c95347b351a1bf918265fbd586e64bb4fe3d7
Zijie Li <michael20001122@gmail.com>
2023-12-27 15:32:45 +0800

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import './App.css'; // 假设你的CSS文件名是ChatInterface.css
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
+  const [reportUrl, setReportUrl] = useState(''); // 新状态来保存报告的URL
+
 
   const sendMessage = async () => {
     const userMessage = {
@@ -32,9 +34,10 @@ const App = () => {
         text: response.data.message,
         sender: 'ai',
       };
+      // 存储报告的URL
+      setReportUrl(response.data.report_url);
     } catch (error) {
       console.error('Error sending message:', error);
-      // 如果请求失败，我们可以设置一个错误消息
       aiMessage = {
         text: 'Error: Could not retrieve the report.',
         sender: 'ai',
@@ -58,6 +61,12 @@ const App = () => {
             {msg.text}
           </div>
         ))}
+        {/* 如果有报告的URL，则显示下载按钮 */}
+        {reportUrl && (
+          <div className="report-download">
+            <a href={reportUrl} download target="_blank" rel="noopener noreferrer">Download Report</a>
+          </div>
+        )}
       </div>
       <div className="input-area">
         <input
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,31 +1,41 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/members")
-# def members():
-#     return {'members': ['John', 'Tom', 'Jerry']}
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-from flask import Flask, request, jsonify
+from flask import Flask, send_file, request, jsonify, send_from_directory
+from reportlab.pdfgen import canvas
+import os
 
 app = Flask(__name__)
+
+# 确保reports文件夹存在，并且是在Flask应用的根目录下
+reports_dir = os.path.join(app.root_path, 'reports')
+os.makedirs(reports_dir, exist_ok=True)
 
 @app.route('/generate_report', methods=['POST'])
 def generate_report():
     data = request.json
     person_name = data.get('person')
     linkedin_url = data.get('linkedin_url')
-    
-    # 这里可以添加逻辑以生成报告，并返回报告的链接
-    report_url = "http://example.com/report.pdf"  # 假设报告的链接
 
+    # 文件路径
+    file_path = os.path.join(reports_dir, f"{person_name}_report.pdf")
+
+    # 创建一个PDF文件并保存到文件系统
+    c = canvas.Canvas(file_path)
+    c.drawString(100, 750, f"Report for: {person_name}")
+    c.drawString(100, 730, f"LinkedIn URL: {linkedin_url}")
+    c.showPage()
+    c.save()
+
+    # 提供一个URL以供下载
+    report_url = f"http://{request.host}/reports/{os.path.basename(file_path)}"
+
+    # 返回AI的回复和下载报告的链接
     return jsonify({
-        'message': f"Okay, here is a one page report on {person_name} ({linkedin_url}), would you like to download the report?",
+        'message': f'Okay, here is a one page report on {person_name}, would you like to download the report?',
         'report_url': report_url
     })
+
+@app.route('/reports/<filename>')
+def report(filename):
+    return send_from_directory(reports_dir, filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
