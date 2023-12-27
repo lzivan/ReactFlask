55156253a8dd79ef4e92f3b2f195a58a324db373
Zijie Li <michael20001122@gmail.com>
2023-12-27 13:19:53 +0800

    无report版

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,75 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'; // 假设你的CSS文件名是ChatInterface.css
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
 
-  const [data, setMessage] = useState([{}])
-
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-        data => 
-        setMessage(data))
-        console.log(data)
-  }, [])
-
+  const sendMessage = async () => {
+    const userMessage = {
+      text: inputMessage,
+      sender: 'user',
+    };
+  
+    // 尝试解析用户消息中的人名和LinkedIn URL
+    const personMatch = inputMessage.match(/\[PERSON: (.*?)\]/);
+    const linkedinUrlMatch = inputMessage.match(/\[LINKEDINURL: (.*?)\]/);
+  
+    const personName = personMatch ? personMatch[1] : null;
+    const linkedinUrl = linkedinUrlMatch ? linkedinUrlMatch[1] : null;
+  
+    // 只定义aiMessage，不要立即添加到状态中
+    let aiMessage;
+  
+    try {
+      // 发送消息到后端并等待响应
+      const response = await axios.post('/generate_report', {
+        person: personName,
+        linkedin_url: linkedinUrl,
+      });
+      aiMessage = {
+        text: response.data.message,
+        sender: 'ai',
+      };
+    } catch (error) {
+      console.error('Error sending message:', error);
+      // 如果请求失败，我们可以设置一个错误消息
+      aiMessage = {
+        text: 'Error: Could not retrieve the report.',
+        sender: 'ai',
+      };
+    }
+  
+    // 一次性更新状态以包括用户消息和AI响应
+    setMessages(prevMessages => [...prevMessages, userMessage, aiMessage]);
+  
+    // 清空输入区域
+    setInputMessage('');
+  };
+  
+  
 
   return (
-    <div>
-      {
-        (typeof data.members === 'undefined') ? (
-          <p>Loading...</p>
-        )   : (
-          data.members.map((member, index) => (
-            <p key={index}>{member}</p>
-          ))
-        )     
-      }
+    <div className="chat-interface">
+      <div className="messages-list">
+        {messages.map((msg, index) => (
+          <div key={index} className={`message ${msg.sender}`}>
+            {msg.text}
+          </div>
+        ))}
+      </div>
+      <div className="input-area">
+        <input
+          type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,31 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/members")
+# def members():
+#     return {'members': ['John', 'Tom', 'Jerry']}
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return {'members': ['John', 'Tom', 'Jerry']}
+@app.route('/generate_report', methods=['POST'])
+def generate_report():
+    data = request.json
+    person_name = data.get('person')
+    linkedin_url = data.get('linkedin_url')
+    
+    # 这里可以添加逻辑以生成报告，并返回报告的链接
+    report_url = "http://example.com/report.pdf"  # 假设报告的链接
+
+    return jsonify({
+        'message': f"Okay, here is a one page report on {person_name} ({linkedin_url}), would you like to download the report?",
+        'report_url': report_url
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
